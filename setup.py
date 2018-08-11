f1043864608157e62bcc5554f27903d9824b6f44
Simon Walker <s.r.walker101@googlemail.com>
2018-08-11 20:34:06 +0100

    Add initial package and tests

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name='youtube-downloader',
+        author='Simon Walker',
+        author_email='s.r.walker101@googlemail.com',
+        packages=find_packages(),
+        )
