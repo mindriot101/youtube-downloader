fb7da69f7a6f46f7f0bafa0d5702b2324db73206
Simon Walker <s.r.walker101@googlemail.com>
2018-08-17 19:06:52 +0100

    Add yt-add program

---
+++ src/bin/yt_add.rs
@@ -0,0 +1,49 @@
+extern crate zmq;
+#[macro_use]
+extern crate structopt;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use structopt::StructOpt;
+use std::error::Error;
+
+#[derive(Debug, Serialize)]
+struct Job {
+    url: String,
+    dest: String,
+}
+
+impl Job {
+    fn new<S>(url: S, dest: S) -> Self
+        where S: Into<String> {
+            Job {
+                url: url.into(),
+                dest: dest.into(),
+            }
+        }
+}
+
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(short = "u", long = "url")]
+    url: String,
+    #[structopt(short = "d", long = "dest")]
+    dest: String,
+}
+
+fn add_job(job: Job) -> Result<(), Box<Error>> {
+    let message = serde_json::to_vec(&job)?;
+    let context = zmq::Context::new();
+    let socket = context.socket(zmq::SocketType::REQ)?;
+    socket.connect("tcp://127.0.0.1:5505")?;
+    socket.send(&message, 0)?;
+
+    Ok(())
+}
+
+fn main() {
+    let opts = Opts::from_args();
+    let job = Job::new(opts.url, opts.dest);
+    add_job(job).unwrap();
+}
