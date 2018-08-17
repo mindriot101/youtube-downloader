97e69e55366b91927174b9fc564201fcd15797af
Simon Walker <s.r.walker101@googlemail.com>
2018-08-17 19:48:54 +0100

    Add argument handling for server

--- src/bin/yt_server.rs
+++ src/bin/yt_server.rs
@@ -8,8 +8,18 @@ extern crate youtube_downloader;
 
 use structopt::StructOpt;
 use std::error::Error;
+use std::path::PathBuf;
 use youtube_downloader::job::Job;
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    #[structopt(short = "c", long = "config", parse(from_os_str))]
+    config: PathBuf,
+    #[structopt(short = "s", long = "sleep-time", default_value = "86400")]
+    sleep_time: u32,
+}
 
 
 fn main() {
+    let opts = Opts::from_args();
 }
